94483b892410cbb13c7c8fc22f059c5db4e6fb11
andreas.larsson <andreas.larsson@connectblue.com>
2016-05-04 18:05:32 +0200

    Added API documentation

--- ublox-odin-w2-drivers/cb_gatt_server.h
+++ ublox-odin-w2-drivers/cb_gatt_server.h
@@ -44,6 +44,27 @@ extern "C" {
  *==============================================================================
  */
 
+/**
+ * Attribute database entry
+ * @param pUuid             16 or 128-bits UUID
+ * @param uuidFormat        Format of the pUuid
+ * @param properties        Properties see cbGATT_PROP_* in cb_gatt.h
+ * @param security          Read/write security properties for this characteristic see cbGATT_WR_SEC_* and cbGATT_RD_SEC_*
+ * @param pvValue1          Depends on pUuid, see below
+ *                          cbGATT_CHAR_EXT_PROP - properties as cb_uint32
+ *                          cbGATT_CLIENT_CHAR_CONFIG - callback that is called when remote device reads the client config, cbGATT_ServerReadClientConfig
+ *                          cbGATT_SERVER_CHAR_CONFIG - callback that is called when remote device reads the server config, cbGATT_ServerReadServerConfig
+ *                          cbGATT_CHAR_FORMAT - Pointer to cbGATT_CharFormat
+ *                          cbGATT_CHAR_USER_DESC and all other CHARACTERISTICS value - cbGATT_ServerReadAttr callback
+ *                          cbGATT_INCLUDE_DECL - Pointer to inlcuded service cbGATT_Attribute
+ * @param pvValue2          Depends on pUuid, see below
+ *                          cbGATT_CLIENT_CHAR_CONFIG - callback that is called when remote device writes the client config cbGATT_ServerWriteClientConfig
+ *                          cbGATT_CLIENT_CHAR_CONFIG - callback that is called when remote device writes the client config cbGATT_ServerWriteClientConfig
+ *                          cbGATT_SERVER_CHAR_CONFIG - callback that is called when remote device writes the server config cbGATT_ServerWriteServerConfig
+ *                          cbGATT_CHAR_USER_DESC and all other CHARACTERISTICS value - cbGATT_ServerWriteAttr callback
+ * @param pAttrHandle       Pointer where to write the attribute handle in case it's needed by the app. 
+ *                          If not needed, pass NULL. Will be written after the service has been added.
+ */
 typedef struct
 {
     void*               pUuid;
@@ -55,23 +76,51 @@ typedef struct
     cb_uint16*          pAttrHandle;
 } cbGATT_Attribute;
 
-
+/**
+ * Callback is called when the indication has been confirmed.
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param errorCode         cbGATT_ERROR_CODE_OK when succeeded
+ */
 typedef void (*cbGATT_CharacteristicValueIndicationCnf)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_uint16           attrHandle,
     cbGATT_ErrorCode    errorCode);
 
+/**
+ * Callback is called when the notification has been sent.
+ * @param connHandle        Connection handle
+ * @param errorCode         cbGATT_ERROR_CODE_OK when succeeded
+ */
 typedef void (*cbGATT_CharacteristicValueNotificationCnf)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cbGATT_ErrorCode    errorCode);
 
+/**
+ * Callback is called when the GATT client has commited a write long
+ * @param connHandle        Connection handle
+ * @param commit            TRUE = commit, FALSE = cancel
+ * @return cbGATT_ERROR_CODE_OK if accepted or some cbGATT_ERROR_CODE_* code when failed.
+ */
 typedef cbGATT_ErrorCode (*cbGATT_CharacteristicWriteLongCommitEvt)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_boolean          commit);
 
 // Callbacks to use in server table
+
+/**
+ * Callback is called when the client is reading an attribute
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param pAttr             Pointer to attribute record
+ * @param pAttrValue        Pointer where to put the read data
+ * @param pLength           Pointer where to put the read length.
+ * @param maxLength         Max number of bytes that is allowed for pAttrValue
+ * @param offset            The offset of the read data
+ * @return cbGATT_ERROR_CODE_OK if accepted or some cbGATT_ERROR_CODE_* code when failed.
+ */
 typedef cbGATT_ErrorCode (*cbGATT_ServerReadAttr)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_uint16           attrHandle,
     cbGATT_Attribute*   pAttr,
     cb_uint8*           pAttrValue,
@@ -79,8 +128,23 @@ typedef cbGATT_ErrorCode (*cbGATT_ServerReadAttr)(
     cb_uint16           maxLength,
     cb_uint16           offset);
 
+/**
+ * Callback is called when the client is writing an attribute.
+ * If the application wants to send the response later it can
+ * return the cbGATT_ERROR_CODE_DELAYED_RSP error code and call cbGATT_writeRsp
+ * when ready.
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param pAttr             Pointer to attribute record
+ * @param pAttrValue        Pointer where to get the data
+ * @param length            The length.
+ * @param writeMethod       Which write method the client is using. 
+ *                          This depends on the properties in the attribute table.
+ * @param offset            The offset of the written data
+ * @return cbGATT_ERROR_CODE_OK if accepted or some cbGATT_ERROR_CODE_* code when failed.
+ */
 typedef cbGATT_ErrorCode (*cbGATT_ServerWriteAttr)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_uint16           attrHandle,
     cbGATT_Attribute*   pAttr,
     cb_uint8*           pAttrValue,
@@ -88,24 +152,56 @@ typedef cbGATT_ErrorCode (*cbGATT_ServerWriteAttr)(
     cbGATT_WriteMethod  writeMethod,
     cb_uint16           offset);
 
+/**
+ * Callback is called when the client is reading the client config
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param pConfig           Pointer where to write the config
+ * @return cbGATT_ERROR_CODE_OK if accepted or some cbGATT_ERROR_CODE_* code when failed.
+ */
 typedef cbGATT_ErrorCode (*cbGATT_ServerReadClientConfig)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_uint16           attrHandle,
     cb_uint16*          pConfig);
 
+/**
+ * Callback is called when the client is writing the client config
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param config            The config to be stored
+ * @param writeMethod       Which write method the client is using. 
+ *                          This depends on the properties in the attribute table.
+ * @return cbGATT_ERROR_CODE_OK if accepted or some cbGATT_ERROR_CODE_* code when failed.
+ */
 typedef cbGATT_ErrorCode (*cbGATT_ServerWriteClientConfig)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_uint16           attrHandle,
     cb_uint16           config,
     cbGATT_WriteMethod  writeMethod);
 
+/**
+ * Callback is called when the client is reading the server config
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param pConfig           Pointer where to write the config
+ * @return cbGATT_ERROR_CODE_OK if accepted or some cbGATT_ERROR_CODE_* code when failed.
+ */
 typedef cbGATT_ErrorCode (*cbGATT_ServerReadServerConfig)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_uint16           attrHandle,
     cb_uint16*          pConfig);
 
+/**
+ * Callback is called when the client is writing the server config
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param config            The config to be stored
+ * @param writeMethod       Which write method the client is using. 
+ *                          This depends on the properties in the attribute table.
+ * @return cbGATT_ERROR_CODE_OK if accepted or some cbGATT_ERROR_CODE_* code when failed.
+ */
 typedef cbGATT_ErrorCode (*cbGATT_ServerWriteServerConfig)(
-    TConnHandle         handle,
+    TConnHandle         connHandle,
     cb_uint16           attrHandle,
     cb_uint16           config,
     cbGATT_WriteMethod  writeMethod);
@@ -125,44 +221,91 @@ typedef struct
  *=============================================================================
  */
 
+/**
+ * Register server callbacks
+ * @param pCallBack         Server callback
+ * @param pAppHandle        Where to store app handle
+ * @return cbGATT_OK if succeeded or cbGATT_ERROR when failed.
+ */
 cb_int32 cbGATT_registerServer(
     const cbGATT_ServerCallBack* pCallBack, 
     cb_uint8*       pAppHandle);
 
+/**
+ * Deregister all server callbacks
+ * @return cbGATT_OK if succeeded or cbGATT_ERROR when failed.
+ */
 cb_int32 cbGATT_deregisterAllServers(void);
 
+/**
+ * Send notification to GATT client. The characteristicValueNotificationCnf
+ * callback will be called when finished and a new notification can be sent.
+ * The client config notification must have been enabled by the GATT client 
+ * before an notification can be sent.
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param pData             Pointer to data to send
+ * @param length            Length of pData
+ * @param appHandle         App handle
+ * @return cbGATT_OK if succeeded or some cbGATT_ERROR* when failed.
+ */
 cb_int32 cbGATT_notification(
-    TConnHandle     handle, 
+    TConnHandle     connHandle, 
     cb_uint16       attrHandle, 
     cb_uint8*       pData, 
     cb_uint16       length, 
     cb_uint8        appHandle);
 
+/**
+ * Send indication to GATT client. The characteristicValueIndicationCnf
+ * callback will be called when finished and a new indication can be sent.
+ * The client config indication must have been enabled by the GATT client 
+ * before an indication can be sent.
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param pData             Pointer to data to send
+ * @param length            Length of pData
+ * @param appHandle         App handle
+ * @return cbGATT_OK if succeeded or some cbGATT_ERROR* when failed.
+ */
 cb_int32 cbGATT_indication(
-    TConnHandle     handle, 
+    TConnHandle     connHandle, 
     cb_uint16       attrHandle, 
     cb_uint8*       pData, 
     cb_uint16       length, 
     cb_uint8        appHandle);
 
-cb_int32 cbGATT_confirmation(
-    TConnHandle     handle, 
-    cb_uint8        appHandle);
-
+/**
+ * Delay write respone to client, see cbGATT_ServerWriteAttr
+ * @param connHandle        Connection handle
+ * @param attrHandle        Handle of the attribute value
+ * @param errorCode         Error code, use cbGATT_ERROR_CODE_OK if OK otherwise some cbGATT_ERROR_CODE_* 
+ * @return cbGATT_OK if succeeded or some cbGATT_ERROR* when failed.
+ */
 cb_int32 cbGATT_writeRsp(
-    TConnHandle     handle,
+    TConnHandle     connHandle,
     cb_uint16       attrHandle, 
     cb_uint8        errorCode); // For delayed write responses
 
-cb_int32 cbGATT_allocServices(
-    cb_uint16       attrListSize);
-
-// Note that startHandle for the application should start at lowest 1024. 1-1023 is reserved for GATT/GAP and other u-blox services
+/**
+ * Add service list to attribute database
+ * @param pAttrList         Attribute list
+ * @param attrListSize      Size of the attribute list
+ * @param startHandle       Start handle. Note that startHandle for the application 
+ *                          should start at lowest 1024. 1-1023 is reserved for 
+ *                          GATT/GAP and other u-blox services.
+ * @return cbGATT_OK if succeeded or some cbGATT_ERROR* when failed.
+ */
 cb_int32 cbGATT_addService(
     const cbGATT_Attribute* pAttrList, 
     cb_uint16       attrListSize, 
     cb_int16        startHandle);
 
+/**
+ * NOTE: Only for tests
+ * Free all services. Use with care since this will also remove GATT and GAP services.
+ * @return cbGATT_OK if succeeded or some cbGATT_ERROR* when failed.
+ */
 cb_int32 cbGATT_freeServices(void);
 
 #ifdef __cplusplus
