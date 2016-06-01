70ef29d3c5c17ca5656f67f014d10086865c452e
andreas.larsson <andreas.larsson@connectblue.com>
2016-06-01 02:09:41 +0200

    Split cbETH_init into cbETH_registerPacketIndicationCallback and cbETH_init to match cbWLAN

--- ublox-odin-w2-drivers/cb_ethernet.h
+++ ublox-odin-w2-drivers/cb_ethernet.h
@@ -13,10 +13,9 @@
 * interface definitions for the Ethernet driver
 *------------------------------------------------------------------------------
 */
-  
 
-#ifndef __ETHERNETIF_H__
-#define __ETHERNETIF_H__
+#ifndef _CB_ETHERNET_H_
+#define _CB_ETHERNET_H_
 
 #include "cb_comdefs.h"
 #include "stm32f4xx_hal.h"
@@ -25,8 +24,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// TODO add documentation
 
 typedef void(*cbETH_packetIndication)(cb_uint8* pBuf, cb_uint32 length);
 
@@ -43,12 +40,41 @@ typedef struct {
     cbETH_MspDeInitCallback mspDeInit;
 } cbETH_HALWeakCallbacks;
 
-void cbETH_init(cb_uint8* pMacAddr, cbETH_packetIndication packetInfo);
+/**
+ * Initialize ethernet driver.
+ * 
+ * @param   pMacAddr      Pointer to MAC address. Must not be NULL.
+ */
+void cbETH_init(cb_uint8* pMacAddr);
 
-cb_boolean cbETH_transmit(cb_uint32 len);
+/**
+ * Register packet indication callback.
+ * 
+ * @param   packetInfo      Callback for received packets.
+ */
+void cbETH_registerPacketIndicationCallback(cbETH_packetIndication packetInfo);
+
+/**
+ * Get the transmit buffer
+ * 
+ * @return   Packet buffer where data to send shall be put.
+ */
 cb_uint8* cbETH_getTransmitBuffer(void);
 
-// Override weak callbacks from HAL. Used when an alternate Ethernet driver shall be used.
+/**
+ * Transmit packet buffer. Call cbETH_getTransmitBuffer
+ * before to get the transmit buffer to put data in.
+ * 
+ * @param    length Length of the ethernet frame to send
+ * @return   TRUE on success otherwise FALSE
+ */
+cb_boolean cbETH_transmit(cb_uint32 length);
+
+/**
+ * Override weak callbacks from HAL. Used when an alternate Ethernet driver shall be used.
+ * 
+ * @param   pCallbacks Pointer to callbacks
+ */
 void cbETH_overrideHALWeakCallbacks(cbETH_HALWeakCallbacks* pCallbacks);
 
 #ifdef __cplusplus
