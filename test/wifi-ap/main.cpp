423bfd000ebc5f5ce081ab0c5206aca9b93f7189
andreas.larsson <andreas.larsson@connectblue.com>
2018-03-05 22:38:25 +0100

    Removed disable80211d

--- test/wifi-ap/main.cpp
+++ test/wifi-ap/main.cpp
@@ -324,7 +324,6 @@ void app_start(int argc, char *argv[]) {
 
     cbWLAN_StartParameters startParams;
     memset(&startParams, 0, sizeof(startParams));
-    startParams.disable80211d = FALSE; // The driver must always support this feature
     startParams.deviceType = cbWM_MODULE_ODIN_W26X; // ODIN-W2 is the only applicable for ARM mbed
     startParams.deviceSpecific.ODIN_W26X.txPowerSettings.lowTxPowerLevel = cbWLAN_TX_POWER_AUTO;
     startParams.deviceSpecific.ODIN_W26X.txPowerSettings.medTxPowerLevel = cbWLAN_TX_POWER_AUTO;
